5f1623c5501541865915a1bc6eb633e2aece6e95
HighestDreams <79803040+HighestDreams@users.noreply.github.com>
2021-09-04 05:38:49 -0700

    Update Main.php

--- src/HighestDreams/DeathChest/Main.php
+++ src/HighestDreams/DeathChest/Main.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types=1);
 
 namespace HighestDreams\DeathChest;
@@ -7,44 +6,43 @@ namespace HighestDreams\DeathChest;
 use pocketmine\block\Block;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\math\Vector3;
 use pocketmine\plugin\PluginBase;
 use pocketmine\tile\Chest as TileChest;
 use pocketmine\tile\Tile;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener
+{
 
     public function onEnable()
     {
-        $this->getLogger()->warning("Kos server nagaidam (Plugin by HighestDreams) Telegram channel => @ErrorByNightGuard");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-    public function onJoin (PlayerJoinEvent $event) {
-        echo ' Joined';
-    }
-
-    public function onDeath (PlayerDeathEvent $event) { // Pedaret mord...
+    /**
+     * @param PlayerDeathEvent $event
+     */
+    public function onDeath(PlayerDeathEvent $event)
+    {
         $player = $event->getPlayer();
-        $drops = $player->getDrops();
 
+        /* Honestly i hate using 1 code in 2 line just for a little stupid change */
         foreach ([0, 1] as $num) {
             $player->getLevel()->setBlock(new Vector3($player->getX() + $num, $player->getY(), $player->getZ()), Block::get(Block::CHEST));
         }
 
-        $tile = Tile::createTile(Tile::CHEST, $player->getLevel(), TileChest::createNBT(new Vector3($player->getX(), $player->getY(), $player->getZ())));
-        $tile2 = Tile::createTile(Tile::CHEST, $player->getLevel(), TileChest::createNBT(new Vector3($player->getX() + 1, $player->getY(), $player->getZ())));
+        $firstChest = Tile::createTile(Tile::CHEST, $player->getLevel(), TileChest::createNBT($player->asVector3()));
+        $secondChest = Tile::createTile(Tile::CHEST, $player->getLevel(), TileChest::createNBT(new Vector3($player->getX() + 1, $player->getY(), $player->getZ())));
 
-        if ($tile instanceof TileChest) {
-            if ($tile2 instanceof TileChest) {
-                $tile->pairWith($tile2);
+        if ($firstChest instanceof TileChest) {
+            if ($secondChest instanceof TileChest) {
+                $firstChest->pairWith($secondChest);
             }
-            foreach ($drops as $drop) {
-                $tile->getInventory()->addItem($drop);
+            foreach ($player->getDrops() as $drop) {
+                $firstChest->getInventory()->addItem($drop);
             }
         }
         $event->setDrops([]);
-        $player->sendMessage("Item haye shoma to chest save shod! (Plugin By HighestDreams)");
+        $player->sendMessage("Your items saved in your loot chest! go back and get your items before other players find your loot chest!");
     }
 }
